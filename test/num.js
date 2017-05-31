c81c5c07f8de7c54aa5321099673cd4e6e83749f
Tobias Schwarz <toschdev@gmail.com>
2017-05-31 15:06:18 +0200

    Adds gitignore, adjusts tests to new return object with strings instead of numbers

--- test/num.js
+++ test/num.js
@@ -16,21 +16,21 @@ test('nums', function (t) {
         z : 1e7,
         w : '10f',
         hex : 0xdeadbeef,
-        _ : [ 789 ]
+        _ : [ '789' ]
     });
     t.deepEqual(typeof argv.x, 'number');
     t.deepEqual(typeof argv.y, 'number');
     t.deepEqual(typeof argv.z, 'number');
     t.deepEqual(typeof argv.w, 'string');
     t.deepEqual(typeof argv.hex, 'number');
-    t.deepEqual(typeof argv._[0], 'number');
+    t.deepEqual(typeof argv._[0], 'string');
     t.end();
 });
 
 test('already a number', function (t) {
     var argv = parse([ '-x', 1234, 789 ]);
-    t.deepEqual(argv, { x : 1234, _ : [ 789 ] });
+    t.deepEqual(argv, { x : 1234, _ : [ '789' ] });
     t.deepEqual(typeof argv.x, 'number');
-    t.deepEqual(typeof argv._[0], 'number');
+    t.deepEqual(typeof argv._[0], 'string');
     t.end();
 });
